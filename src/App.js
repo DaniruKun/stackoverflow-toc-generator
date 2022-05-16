7efa2dc377212ccb82107f7cc4cba324413a51ce
Daniils Petrovs <thedanpetrov@gmail.com>
2022-05-17 01:26:34 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button, Form } from 'react-bootstrap'
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MarkdownGenerator></MarkdownGenerator>
   );
+}
+
+class MarkdownGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputMd: '' };
+  }
+
+  logStuff(e) {
+    e.preventDefault()
+    console.log('button pressed')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <h2>Stackoverflow Table of Contents generator</h2>
+
+          <Form onSubmit={this.logStuff}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Stackoverflow Markdown</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="Paste Markdown HERE" />
+            </Form.Group>
+            <Button variant="primary" type="submit">Generate</Button>
+          </Form>
+
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
