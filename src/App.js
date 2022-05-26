bea8611a44a5f515a304f7ce5e22c59d81eace70
Daniils Petrovs <thedanpetrov@gmail.com>
2022-05-26 16:24:28 +0200

    Handle article edit URLs

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class MarkdownGenerator extends React.Component {
   }
 
   handleChangeURL(e) {
-    this.setState({ articleURL: e.target.value })
+    this.setState({ articleURL: cleanURL(e.target.value) })
   }
 
   handleSubmit(e) {
@@ -68,17 +68,17 @@ class MarkdownGenerator extends React.Component {
   render() {
     return (
       <div className="App m-3">
-          <h2>Stackoverflow Table of Contents generator</h2>
+        <h2>Stackoverflow Table of Contents generator</h2>
 
-          <Form onSubmit={this.handleSubmit} className='w-75'>
-            <Form.Group className="mb-3" controlId="markdownForm.ControlTextarea1">
-              <Form.Control as="textarea" rows={3} className="my-3 form-control-lg" placeholder="Paste Markdown HERE" onChange={this.handleChangeMd} />
-              <Form.Control type="url" placeholder='Paste Stackoverflow article URL' onChange={this.handleChangeURL}></Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit">Generate</Button>
-          </Form>
+        <Form onSubmit={this.handleSubmit} className='w-75'>
+          <Form.Group className="mb-3" controlId="markdownForm.ControlTextarea1">
+            <Form.Control as="textarea" rows={3} className="my-3 form-control-lg" placeholder="Paste Markdown HERE" onChange={this.handleChangeMd} autofocus="true" />
+            <Form.Control type="url" placeholder='Paste Stackoverflow article URL' onChange={this.handleChangeURL}></Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">Generate</Button>
+        </Form>
 
-          <Result text={this.state.outputMd}></Result>
+        <Result text={this.state.outputMd}></Result>
       </div>
     )
   }
@@ -86,6 +86,16 @@ class MarkdownGenerator extends React.Component {
 
 function Result(props) {
   return <textarea value={props.text} readOnly className='mt-4 w-50 py-5'></textarea>
+}
+
+function cleanURL(url) {
+  let rawURL = new URL(url)
+  let cleanPath = rawURL.pathname
+    .split('/')
+    .filter(part => part !== 'edit')
+    .join('/')
+  rawURL.pathname = cleanPath
+  return rawURL.toString()
 }
 
 export default App;
